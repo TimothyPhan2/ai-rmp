fad9daa50120bcbc0602cb0d6de3e9d26a1719c9
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-08-26 16:00:13 -0700

    saved embeddings to pinecone

--- ai-rmp-python/setup_rag.py
+++ ai-rmp-python/setup_rag.py
@@ -1,15 +1,54 @@
 from dotenv import load_dotenv
 load_dotenv()
 from pinecone import Pinecone, ServerlessSpec
-from groq import Groq
 import os
 import json
-
+import requests
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
 pc.create_index(
     name="rag",
-    dimension=1536,
+    dimension=384,
     metric="cosine",
     spec=ServerlessSpec(cloud="aws", region="us-east-1"),
 )
+
+model_id="sentence-transformers/all-MiniLM-L6-v2"
+hf_token=os.getenv("HF_TOKEN")
+
+data = json.load(open("reviews.json"))
+processed_data = []
+
+
+
+def get_embedding(text):
+    api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+    headers = {"Authorization": f"Bearer {hf_token}"}
+
+    response = requests.post(api_url, headers=headers, json={"inputs": text, "options":{"wait_for_model":True}})
+    return response.json()
+
+for review in data["profReviews"]:
+    response = get_embedding(review["review"])
+   
+    processed_data.append(
+        {
+            "values": response,
+            "id": review["professor"],
+            "metadata": {
+                "review": review["review"],
+                "course": review["course"],
+                "stars": review["stars"],
+            }
+
+        }
+    )
+index = pc.Index("rag")
+upsert_response = index.upsert(
+        vectors=processed_data,
+        namespace="ns1"
+    )
+print(f"Upserted count: {upsert_response['upserted_count']}")
+
+    # Print index statistics
+print(index.describe_index_stats())
